d2e9144895c36cc0d03f0b7ce1fd2232f89a9d9e
Alexandr <valverin95@gmail.com>
2020-08-09 02:18:00 +0300

    feat: add header module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,18 @@ import '../polyfills';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 
-// NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 import { HomeModule } from './home/home.module';
 
 import { AppComponent } from './app.component';
-import { FoldersModule } from './main/folders/folders.module';
+import { HeaderModule } from '@app/main/header/header.module';
+import { FoldersModule } from '@app/main/folders/folders.module';
 
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,16 +25,10 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     HttpClientModule,
     CoreModule,
     SharedModule,
+    HeaderModule,
     HomeModule,
     FoldersModule,
     AppRoutingModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
